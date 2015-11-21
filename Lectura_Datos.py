15ae5722fc879142e0c2171dcb10778fc16a6cb0
SociosComputacion <mario.garciamontero@gmail.com>
2015-11-21 10:57:24 +0100

    Fichero de lectura de datos del Cupón
    El fichero incluye: lectura de datos, comprobación de premio y
    definiciones de librería flask.

---
+++ Lectura_Datos.py
@@ -0,0 +1,59 @@
+import re
+import urllib
+
+f = urllib.urlopen("http://www.resultados11.es/")
+data = f.read()
+f.close()
+
+#print(data)
+
+pattern = re.compile('<li>(\d)</li>\s*<li>(\d)</li>\s*<li>(\d)</li>\s*<li>(\d)</li>\s*<li>(\d)</li>')
+premiado = pattern.findall(data)
+
+#print(premiado)
+
+valores = str(premiado)
+
+valores_ok = valores.split("'")
+
+val1 = int(valores_ok[1])
+val2 = int(valores_ok[3])
+val3 = int(valores_ok[5])
+val4 = int(valores_ok[7])
+val5 = int(valores_ok[9])
+
+
+print val1, val2, val3, val4, val5
+
+
+cifra1 = int(input("introduce cifra 1: "))
+while cifra1 > 9:
+        cifra1 = int(input("ERROR: introduce cifra 1: "))
+cifra2 = int(input("introduce cifra 2: "))
+while cifra2 > 9:
+        cifra2 = int(input("ERROR: introduce cifra 1: "))
+cifra3 = int(input("introduce cifra 3: "))
+while cifra3 > 9:
+        cifra3 = int(input("ERROR: introduce cifra 1: "))
+cifra4 = int(input("introduce cifra 4: "))
+while cifra4 > 9:
+        cifra4 = int(input("ERROR: introduce cifra 1: "))
+cifra5 = int(input("introduce cifra 5: "))
+while cifra5 > 9:
+        cifra5 = int(input("ERROR: introduce cifra 1: "))
+print("TU BOLETO="), cifra1, cifra2, cifra3, cifra4, cifra5
+
+if cifra1 == val1 and cifra2 == val2 and cifra3 == val3 and cifra4 == val4 and cifra5 == val5:
+    print("PREMIO = EL GORDO SOCIOOOOOOO")
+elif cifra2 == val2 and cifra3 == val3 and cifra4 == val4 and cifra5 == val5:
+    print("PREMIO = UUUUUY!! LAS CUATRO ULTIMAS")
+elif cifra3 == val3 and cifra4 == val4 and cifra5 == val5:
+    print("PREMIO = TRES ULTIMAS")
+elif cifra4 == val4 and cifra5 == val5:
+    print("PREMIO = DOBLE REINTEGRO")
+elif cifra5 == val5:
+    print("PREMIO = REINTEGRO")
+else:
+    print("OTRA VEZ SERA")
+
+
